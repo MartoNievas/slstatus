9613181f3469357e7c41fb97af4c4a79f360b33d
Martin Nievas <tinnievas@gmail.com>
2026-04-25 12:21:03 -0300

    The module macro was modified to add the /home/martin variable to the main path.

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 
-#define MODULE(name) "/home/martin/dev/suckless-btw/scripts/" name
+#define MODULE(name) "$HOME/dev/suckless-btw/scripts/" name
 
 #if defined(BUILD_DESKTOP)
 
@@ -85,16 +85,18 @@ static const struct arg args[] = {
      "xkb-switch -p | tr '[:lower:]' '[:upper:]'"},
 
     /* Volumen */
-{ run_command, "\x06 ^b#16161e^^c#ff9e64^ %s ", 
-  "pactl get-default-sink | grep -Ei 'hyperx|usb' >/dev/null && echo '󰋋' || echo '󰓃'" },
+    {run_command, "\x06 ^b#16161e^^c#ff9e64^ %s ",
+     "pactl get-default-sink | grep -Ei 'hyperx|usb' >/dev/null && echo '󰋋' "
+     "|| echo '󰓃'"},
     {run_command, "\x06 | ^b#16161e^^c#ff9e64^  %4s ^d^ ",
      "pamixer --get-volume-human"},
 
     /* BLOQUE HARDWARE: CPU | RAM | DISCO | TEMP */
     {cpu_perc, "\x05 ^b#16161e^^c#2ac3de^  %s%% ", NULL},
     /* CAMBIADO: AMD Ryzen 5 1600 AF via k10temp (Tdie) */
-{run_command, "\x04 |  %s°C ",
-     "sensors k10temp-pci-00c3 2>/dev/null | awk '/Tctl|Tdie/{print $2}' | sed 's/+//;s/°C//' | head -n1"},
+    {run_command, "\x04 |  %s°C ",
+     "sensors k10temp-pci-00c3 2>/dev/null | awk '/Tctl|Tdie/{print $2}' | sed "
+     "'s/+//;s/°C//' | head -n1"},
     {ram_perc, "\x03 |  %s%% ", NULL},
     {disk_perc, "\x02 |  %s%% ^d^ ", "/"},
 
@@ -106,40 +108,40 @@ static const struct arg args[] = {
 #else
 
 static const struct arg args[] = {
-  /* function          format                                     argument */
-  /* Bluetooth & Discord */
-  {run_command, "\x0c ^b#16161e^^c#7aa2f7^ %s ", MODULE("discord-status.sh")},
-  {run_command, "\x0b | %s ", MODULE("bluetooth-status.sh")},
+    /* function          format                                     argument */
+    /* Bluetooth & Discord */
+    {run_command, "\x0c ^b#16161e^^c#7aa2f7^ %s ", MODULE("discord-status.sh")},
+    {run_command, "\x0b | %s ", MODULE("bluetooth-status.sh")},
 
-  /* Red (Nuevo bloque) */
-  /* Mostramos icono de red y el nombre de la conexión activa */
-  {run_command, "\x0a | 󰖩 %s ^d^ ",
-    "nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2"},
+    /* Red (Nuevo bloque) */
+    /* Mostramos icono de red y el nombre de la conexión activa */
+    {run_command, "\x0a | 󰖩 %s ^d^ ",
+     "nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2"},
 
-  /* Teclado */
-  {run_command, "\x09 ^b#16161e^^c#a9b1d6^  %s ^d^ ",
-    "xkb-switch -p | tr '[:lower:]' '[:upper:]'"},
+    /* Teclado */
+    {run_command, "\x09 ^b#16161e^^c#a9b1d6^  %s ^d^ ",
+     "xkb-switch -p | tr '[:lower:]' '[:upper:]'"},
 
-  /* Volumen */
-  {run_command, "\x08 ^b#16161e^^c#ff9e64^  %4s ^d^ ",
-    "pamixer --get-volume-human"},
+    /* Volumen */
+    {run_command, "\x08 ^b#16161e^^c#ff9e64^  %4s ^d^ ",
+     "pamixer --get-volume-human"},
 
-  /* BLOQUE HARDWARE: CPU | RAM | DISCO | TEMP */
-  {cpu_perc, "\x07 ^b#16161e^^c#2ac3de^  %s%% ", NULL},
-  /* CAMBIADO: Intel via sysfs en lugar de sensors */
-  {run_command, "\x06 |  %s°C ",
-    "cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"%d\", "
-      "$1/1000}'"},
-  {ram_perc, "\x05 |  %s%% ", NULL},
-  {disk_perc, "\x04 |  %s%% ^d^ ", "/"},
+    /* BLOQUE HARDWARE: CPU | RAM | DISCO | TEMP */
+    {cpu_perc, "\x07 ^b#16161e^^c#2ac3de^  %s%% ", NULL},
+    /* CAMBIADO: Intel via sysfs en lugar de sensors */
+    {run_command, "\x06 |  %s°C ",
+     "cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"%d\", "
+     "$1/1000}'"},
+    {ram_perc, "\x05 |  %s%% ", NULL},
+    {disk_perc, "\x04 |  %s%% ^d^ ", "/"},
 
-  /* Batería */
-  {battery_icon, "\x03 ^b#16161e^^c#f7768e^ %s ", "BAT1"},
-  {battery_perc, "\x02 %s%% ^d^ ", "BAT1"},
+    /* Batería */
+    {battery_icon, "\x03 ^b#16161e^^c#f7768e^ %s ", "BAT1"},
+    {battery_perc, "\x02 %s%% ^d^ ", "BAT1"},
 
-  /* Fecha y Hora */
-  {datetime, "\x01 ^b#16161e^^c#c0caf5^ %s [ ", "%a %b %e"},
-  {datetime, "%s ] ^d^", "%H:%M:%S "},
+    /* Fecha y Hora */
+    {datetime, "\x01 ^b#16161e^^c#c0caf5^ %s [ ", "%a %b %e"},
+    {datetime, "%s ] ^d^", "%H:%M:%S "},
 };
 
 #endif
